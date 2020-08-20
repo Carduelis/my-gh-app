35efeeedd8dff1818f5c8262cd613472b9096d61
Pavel <pavepy@gmail.com>
2020-08-20 17:57:34 +0200

    Updates

---
+++ precache-manifest.de5408d382206b083386ac0850889600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f213791e7a730fc1dc791a3137595e",
+    "url": "/my-gh-app/index.html"
+  },
+  {
+    "revision": "16d12508c6e74524fcba",
+    "url": "/my-gh-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99226a3c5be549807422",
+    "url": "/my-gh-app/static/js/2.85e1dac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-gh-app/static/js/2.85e1dac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d12508c6e74524fcba",
+    "url": "/my-gh-app/static/js/main.e1c76be8.chunk.js"
+  },
+  {
+    "revision": "9280cd2313b0b73a4331",
+    "url": "/my-gh-app/static/js/runtime-main.45502d59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-gh-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
